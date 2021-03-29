185f0e72181536e16818a3f6623d7456a50b148c
Franco Portin <fportin350@gmail.com>
2021-03-29 07:01:22 -0700

    removed console logs

--- backend/app.js
+++ backend/app.js
@@ -56,7 +56,6 @@ app.use((err, _req, _res, next) => {
 // Error formatter
 app.use((err, _req, res, _next)=> {
     res.status(err.status || 500);
-    console.log(err);
     res.json({
         title: err.title || 'Server Error',
         message: err.message,
